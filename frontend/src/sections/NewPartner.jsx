f052cb1a8d4382214e94f437523ea448449e2c7a
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-04-19 17:14:32 +0530

    newly styled, matched theme

--- frontend/src/sections/NewPartner.jsx
+++ frontend/src/sections/NewPartner.jsx
@@ -8,20 +8,20 @@ const NewPartner = () => {
   const { partnerName, setPartnerName, partnerAddress, setPartnerAddress, partnerCity, setPartnerCity, partnerZip, setPartnerZip, partnerCountry, setPartnerCountry, handlePartnerSubmit } = useContext(DataContext);
 
   return (
-    <section className="w-full rounded-[20px] shadow-3xl px-8 py-4 mt-5">
+    <section className="w-full rounded-[20px] shadow-3xl px-8 py-4 mt-5 bg-gray-100">
 
       <Helmet>
         <title>New Partner - Invoicely</title>
       </Helmet>
 
-      <h1 className="text-2xl font-bold mb-4">New Partner</h1>
+      <h1 className="text-2xl font-bold mb-4 text-gray-800">New Partner</h1>
 
       <form onSubmit={handlePartnerSubmit}>
         <div className='flex flex-col md:flex-row gap-10'>
 
           <div className='w-full md:w-1/2'>
             <fieldset>
-              <label htmlFor='name'>Name</label>
+              <label htmlFor='name' className='text-gray-700'>Name</label>
               <input
                 type='text'
                 className='input mt-1'
@@ -33,7 +33,7 @@ const NewPartner = () => {
             </fieldset>
 
             <fieldset className='mt-3'>
-              <label htmlFor='city'>City</label>
+              <label htmlFor='city' className='text-gray-700'>City</label>
               <input
                 type='text'
                 required
@@ -45,7 +45,7 @@ const NewPartner = () => {
             </fieldset>
 
             <fieldset className='mt-3'>
-              <label htmlFor='country'>Country</label>
+              <label htmlFor='country' className='text-gray-700'>Country</label>
               <input
                 type='text'
                 required
@@ -59,7 +59,7 @@ const NewPartner = () => {
 
           <div className='w-full md:w-1/2'>
             <fieldset>
-              <label htmlFor='address'>Address</label>
+              <label htmlFor='address' className='text-gray-700'>Address</label>
               <input
                 type='text'
                 required
@@ -71,7 +71,7 @@ const NewPartner = () => {
             </fieldset>
 
             <fieldset className='mt-3'>
-              <label htmlFor='zip'>Zip</label>
+              <label htmlFor='zip' className='text-gray-700'>Zip</label>
               <input
                 type='number'
                 required
@@ -85,11 +85,11 @@ const NewPartner = () => {
 
         </div>
 
-        <button className='mt-7 px-5 py-3 border font-montserrat text-sm leading-none bg-slate-400 hover:bg-coral-red rounded-xl text-white' type="submit">Save</button>
+        <button className='mt-7 px-5 py-3 border font-montserrat text-sm leading-none bg-teal-500 hover:bg-teal-600 rounded-xl text-white' type="submit">Save</button>
 
       </form>
     </section>
   )
 }
 
-export default NewPartner
+export default NewPartner;
